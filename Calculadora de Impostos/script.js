e4d6eba4ac6aa4a031979ed1de2e0ec25c61bdcb
leonardo cândido12 <leonardocandido1211@gmail.com>
2022-12-15 01:03:23 -0300

    atualização

--- Calculadora de Impostos/script.js
+++ Calculadora de Impostos/script.js
@@ -6,15 +6,16 @@ const tabela = [
     { salinicio: 16000.1, salfim: 19000, salresid: 3000, incide: 0.13 },
     { salinicio: 19000.1, salfim: 35000, salresid: 16000, incide: 0.135 },
     { salinicio: 35000, salfim: 100000000, salresid: 965000000, incide: 0.14 },
-];
-const efetivoHTML = document.getElementById("salario-efetivo");
-const comissHTML = document.getElementById("salario-comiss");
-const exibirIPER = document.getElementById("exibir-iper");
-const exibirINSS = document.getElementById("exibir-inss");
+]
+const efetivoHTML = document.getElementById("salario-efetivo")
+const comissHTML = document.getElementById("salario-comiss")
+const numDepComissHTML = document.getElementById('número-dep-comiss')
+const numDepEfetivoHTML = document.getElementById('número-dep-efetivo')
+const exibirIPER = document.getElementById("exibir-iper")
+const exibirINSS = document.getElementById("exibir-inss")
 const exibirIrEfetivo = document.getElementById('exibir-ir-efetivo')
 const exibirIrComiss = document.getElementById('exibir-ir-comiss')
 const idsIPER = getIdsIPER();
-const numDep = 0
 function getIdsIPER() {
     const ids = [];
     const base = "iper-faixa-";
@@ -27,15 +28,15 @@ function getIdsIPER() {
 }
 
 function onExecutarEfetivo() {
-    const valorEfetivo = efetivoHTML.value.replace(",", ".");
+    const valorEfetivo = efetivoHTML.value.replace(",", ".")
     const IPERs = calcularIPER(valorEfetivo);
-
+    const numDepEfetivo = numDepEfetivoHTML.value 
     let IPER = 0;
     try {
         IPER = IPERs.reduce((a, b) => a + b);
     } catch (error) {}
-    const IRRF = calcularIRPF(valorEfetivo,IPER,numDep)
-    displayEfetivo(IPER, IPERs, IRRF);
+    const IRRF = calcularIRPF(valorEfetivo,IPER,numDepEfetivo)
+    displayEfetivo(IPER, IPERs, IRRF)
    
 }
 
@@ -83,10 +84,11 @@ function displayEfetivo(IPER, IPERs, IRRF) {
 }
 
 function onExecutarComiss() {
-    const valorComiss = comissHTML.value;
-    const INSS = calcularINSS(valorComiss);
-    const IRRF = calcularIRPF(valorComiss,INSS,numDep)
-    displayComiss(INSS, IRRF);
+    const valorComiss = comissHTML.value.replace(",",".")
+    const numDepComiss = numDepComissHTML.value
+    const INSS = calcularINSS(valorComiss)
+    const IRRF = calcularIRPF(valorComiss,INSS,numDepComiss)
+    displayComiss(INSS, IRRF)
 }
 
 function displayComiss(inss,IRRF) {
@@ -121,8 +123,8 @@ function calcularINSS(salario) {
 }
 
 function calcularIRPF(salario, previdencia, numDep){
-    let irrf = 0;
-    let valorMensal = (salario - previdencia) - (numDep * 189.59);
+    let irrf = 0
+    let valorMensal = (salario - previdencia) - (numDep * 189.59)
 
     if(valorMensal > 0.01 && valorMensal <= 1903){
         irrf = (valorMensal * 0.000)
